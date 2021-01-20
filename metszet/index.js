8edbc21cedf69407e963a383762fe217d7c23c5f
huncut2016 <zoller.andras2005@gmail.com>
2021-01-20 09:32:41 +0100

    metszet

---
+++ metszet/index.js
@@ -0,0 +1,85 @@
+let arr1 = [-1, 0, 2, 5, 9, 10];
+let arr2 = [0, 2, 4, 5, 6, 7, 9];
+let Markers1Container = document.getElementById("markersA");
+let elements1Container = document.getElementById("elemsA");
+
+let Markers2Container = document.getElementById("markersB");
+let elements2Container = document.getElementById("elemsB");
+
+
+let metszet;
+generate();
+
+
+function generate(){
+	metszet = new Metszet(arr1, arr2);
+	createTable(elements1Container, Markers1Container, arr1, "1", '<th class="tg-ycr8">A elemei</th>', '<th class="tg-ycr8">A pointere</th>');
+	createTable(elements2Container, Markers2Container, arr2, "2", '<th class="tg-ycr8">B elemei</th>', '<th class="tg-ycr8">B pointere</th>');
+	createTable(document.getElementById("results"), "", [], "", '<th class="tg-ycr8">Jelenlegi állás</th>', "");
+}
+
+
+function createTable(elementElements, elementMarker, array, costumID, labelElems, labelMarkers) {
+    let bodyElemnts = "";
+    let bodyMarkers = "";
+
+    for (let i = 0; i < array.length; i++) {
+        bodyElemnts += `<th class="tg-ycr8" id=${i}>${array[i]}</th>`;
+        if (i == 0) {
+            bodyMarkers += `<th class="tg-ycr8 marker${costumID}" id=${costumID}marker${i}><span class="marker">&#8593</span></th>`;
+
+        } else {
+
+            bodyMarkers += `<th class="tg-ycr8 markers marker${costumID}" id=${costumID}marker${i}><span class="marker">&#8593</span></th>`;
+        }
+
+    }
+
+    elementElements.innerHTML = labelElems + bodyElemnts;
+    elementMarker.innerHTML = labelMarkers + bodyMarkers;
+
+}
+
+document.getElementById("next").onclick = function() {
+    metszet.update();
+    let state = metszet.show();
+    let markersA = document.getElementsByClassName("marker1");
+    let markersB = document.getElementsByClassName("marker2");
+    alert(state.comparison);
+
+    for (const elem of markersA) {
+        if (elem.id == '1marker' + state.pointerA) {
+            elem.classList.remove("markers");
+        } else {
+            elem.classList.add("markers");
+        }
+    }
+
+    for (const elem of markersB) {
+        if (elem.id == '2marker' + state.pointerB) {
+            elem.classList.remove("markers");
+        } else {
+            elem.classList.add("markers");
+        }
+    }
+    createTable(document.getElementById("results"), "", state.currentResult, "", '<th class="tg-ycr8">Jelenlegi állás</th>', "");
+}
+
+document.getElementById("reset").onclick = function() {
+    metszet = new Metszet(arr1, arr2);
+    createTable(elements1Container, Markers1Container, arr1, "1", '<th class="tg-ycr8">A elemei</th>', '<th class="tg-ycr8">A pointere</th>');
+    createTable(elements2Container, Markers2Container, arr2, "2", '<th class="tg-ycr8">B elemei</th>', '<th class="tg-ycr8">B pointere</th>');
+    createTable(document.getElementById("results"), "", [], "", '<th class="tg-ycr8">Jelenlegi állás</th>', "");
+
+}
+
+document.getElementById("addArrays").onclick = function () {
+    let inputArray1 = eval(prompt("Írd be az A tömböt! (példa: [1,2,3,4])"));
+    let inputArray2 = eval(prompt("Írd be a B tömböt!(példa: [1,2,3,4])"));
+
+    arr1 = inputArray1.sort((a, b) => a - b);
+    arr2 = inputArray2.sort((a, b) => a - b);
+
+    generate();
+    
+}
---
+++ metszet/metszet.js
@@ -0,0 +1,61 @@
+class Metszet {
+    constructor(a = [], b = []) {
+        this.a = a;
+        this.b = b;
+        this.result = [];
+
+        this.aSize = a.length;
+        this.bSize = b.length;
+        this.i = 0;
+        this.j = 0;
+        this.node1 = "";
+
+        this.con = true;
+    }
+
+    update() {
+        let aLast = this.aSize - 1;
+        let bLast = this.bSize - 1;
+
+        if (((this.i < this.aSize) || (this.j < this.bSize)) && this.con) {
+            switch (true) {
+                case aLast < this.i || bLast < this.j:
+                    this.node1 = 'Az egyik tömb kifogyott, magyarul nem lehet több megegyező elem. Kilépünk. ';
+                    for (let k = this.j; k < this.bSize; k++) {
+                        this.result.push(this.b[k]);
+                    }
+                    this.con = false;
+                    this.i = this.aSize;
+                    this.j = this.bSize;
+                    break;
+                case this.a[this.i] == this.b[this.j]:
+                    this.node1 = "Mindkét Pointer fölött álló szám egyenlő. Az egyiket belerekjuk a jelenlegi állásba és mindkét Pointert továbbléptetjük";
+                    this.result.push(this.a[this.i]);
+                    this.j++;
+                    this.i++;
+                    break;
+
+                case this.a[this.i] < this.b[this.j]:
+                    this.node1 = "Az APointer fölött álló szám kissebb, mint a BPointer fölött álló szám. (Nem egyenlőek) EZért APointert továbbléptetjük.";
+                    this.i++;
+                    break;
+
+                case this.b[this.j] < this.a[this.i]:
+                    this.node1 = "BPointer fölött álló szám kissebb, mint az APointer fölött álló. (nem egyenlőek) ."
+                    this.j++;
+                    break;
+            }
+        }
+    }
+
+    show() {
+        return {
+            pointerA: this.i,
+            pointerB: this.j,
+            comparison: this.node1,
+            currentResult: this.result,
+            setA: this.a,
+            setB: this.b,
+        }
+    }
+}
