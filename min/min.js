6185646b902c05bc83a1af3545d177a7c631310c
huncut2016 <zoller.andras2005@gmail.com>
2021-01-22 15:50:29 +0100

    min

---
+++ min/min.js
@@ -0,0 +1,33 @@
+class MIN {
+	constructor(a=[]){
+		this.a = a;
+		this.i = 1;
+		this.result = a[0];
+		this.node1 = "";
+	}
+
+	update(){
+
+		if(this.i < this.a.length){
+			if(this.a[this.i] < this.result){
+				this.result = this.a[this.i];
+				this.node1 = "A pointer fölött álló szám kisebb, mint a jelenlegi állás, ezért a pointer fölött álló elemet belerakjuk a jelenlegi állásba és tövábbléptetünk.";
+			}else{
+				this.node1 = "A pointer fölött álló szám nagyobb, mint a jelenlegi állás, ezért továbblépünk."
+
+			}
+			this.i++;
+		}else{
+			this.node1 = "vége";
+		}
+	}
+
+	show () {
+		return {
+			pointerA: this.i,
+			currentResult: [this.result],
+			comparison: this.node1
+		}
+
+	}
+}
